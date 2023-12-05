ae77064de9a5c030f639817dbf43dc7e1e320a71
Karl Maroun <karlmaroun574@gmail.com>
2023-12-05 12:58:58 +0100

    script example for launching lsurm jobs

--- job.sh
+++ job.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+#SBATCH -J pod
+#SBATCH -t 01:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --partition=imb,imb-resources,gpu
+
+
+# useful informations to print
+echo "#############################" 
+echo "User:" $USER
+echo "Date:" `date`
+echo "Host:" `hostname`
+echo "Directory:" `pwd`
+echo "SLURM_JOBID:" $SLURM_JOBID
+echo "SLURM_SUBMIT_DIR:" $SLURM_SUBMIT_DIR
+echo "SLURM_JOB_NODELIST:" $SLURM_JOB_NODELIST
+echo "#############################" 
+
+#############################
+
+mpirun -np 32 python loadsnap.py
